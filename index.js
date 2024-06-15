50929eb1fa0fb2d68253b99627d28fb4a76930bc
alexandreleriche141282 <alexandreleriche141282@gmail.com>
2024-06-15 16:29:30 +0200

    make services

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ function animateText() {
 document.addEventListener('DOMContentLoaded', animateText);
 // --------------------------------------------------------
 
+// Slider --------------- //
+
 const slides = document.querySelectorAll('.slide');
 let currentSlide = 0;
 
@@ -34,8 +36,43 @@ function nextSlide() {
   showSlide(currentSlide);
 }
 
-setInterval(nextSlide, 5000); // Changement d'image toutes les 4 secondes
+setInterval(nextSlide, 5000); // Changement d'image toutes les 5 secondes
 showSlide(currentSlide); // Afficher la première image au chargement
+//  --------------------------------------------------------------
+
+// Initialisation de ScrollReveal
+ScrollReveal().reveal('.section1', {
+  origin: 'left',
+  distance: '100px',
+  duration: 1000,
+  delay: 200,
+  reset: false
+});
+
+ScrollReveal().reveal('.section2', {
+  origin: 'right',
+  distance: '100px',
+  duration: 1000,
+  delay: 400,
+  reset: false
+});
+
+ScrollReveal().reveal('.section3', {
+  origin: 'left',
+  distance: '100px',
+  duration: 1000,
+  delay: 600,
+  reset: false
+});
+
+ScrollReveal().reveal('.section4', {
+  origin: 'right',
+  distance: '100px',
+  duration: 1000,
+  delay: 800,
+  reset: false
+});
+
 
 
 
